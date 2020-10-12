7eb36bc28ef36701445786800c6cee744bde8e27
Arun Kumar <meghas@chetu.com>
2020-10-12 10:12:58 +0530

    Initial

--- src/Left.js
+++ src/Left.js
@@ -7,8 +7,21 @@ class Left extends React.Component {
         portugal: [{ name: 'Aasiya Jayaent', checked: false }, { name: 'Rey Miboure', checked: false }, { name: 'Cyla Brister', checked: false }],
 
     };
-    ChangeValue = (e) => {
-        console.log("Console", e);
+    ChangeValue = (index, name) => {
+        if (name === 'portugal') {
+            const { portugal } = this.state;
+            portugal[index].checked = !portugal[index].checked;
+            this.setState({ portugal });
+
+        } else if (name === 'nicaragua') {
+            const { nicaragua } = this.state;
+            nicaragua[index].checked = !nicaragua[index].checked;
+            this.setState({ nicaragua });
+        } else {
+            const { marshell } = this.state;
+            marshell[index].checked = !marshell[index].checked;
+            this.setState({ marshell });
+        }
     }
 
     render() {
@@ -17,37 +30,69 @@ class Left extends React.Component {
             <div>
                 <div style={{ float: "left", width: "50%" }}>
                     <h3>Portugal</h3>
-                    {portugal.map(data => {
+                    {portugal.map((data, index) => {
                         return (
                             <p>
-                                <input type="checkbox" checked={data.checked} onChange={(e) => this.ChangeValue(e, portugal)} />
+                                <input type="checkbox" checked={data.checked} onChange={(e) => this.ChangeValue(index, 'portugal')} />
                                 <label>{data.name}</label>
                             </p>
                         );
                     })}
                     <h3>Nicargua</h3>
-                    {nicaragua.map(data => {
+                    {nicaragua.map((data, index) => {
                         return (
                             <p>
-                                <input type="checkbox" checked={data.checked} onChange={(e) => this.ChangeValue(e, nicaragua)} />
+                                <input type="checkbox" checked={data.checked} onChange={(e) => this.ChangeValue(index, 'nicaragua')} />
                                 <label>{data.name}</label>
                             </p>
                         );
                     })}
                     <h3>Marshell</h3>
-                    {marshell.map(data => {
+                    {marshell.map((data, index) => {
                         return (
                             <p>
-                                <input type="checkbox" checked={data.checked} onChange={(e) => this.ChangeValue(e, marshell)} />
+                                <input type="checkbox" checked={data.checked} onChange={(e) => this.ChangeValue(index, 'marshell')} />
                                 <label>{data.name}</label>
                             </p>
                         );
-                    })}/
+                    })}
                 </div>
                 <div style={{ float: "left", width: "50%" }}>
-                    <h3>Right Side</h3>
-                    <input type="checkbox" label="Label" onClick={(e) => this.ChangeValue(e)} />
-                    <label>Hello</label>
+                    <h3>Portugal</h3>
+                    {portugal.map((data, index) => {
+                        if (data.checked) {
+                            return (
+                                <p>
+                                    <label style={{ marginRight: "20px" }}>{data.name}</label>
+                                    <button type="button" onClick={() => this.ChangeValue(index, 'portugal')}>Clear</button>
+                                </p>
+                            );
+                        }
+
+                    })}
+                    <h3>Nicargua</h3>
+                    {nicaragua.map((data, index) => {
+                        if (data.checked) {
+                            return (
+                                <p>
+                                    <label>{data.name}</label>
+                                    <button type="button" onClick={() => this.ChangeValue(index, 'nicaragua')}>Clear</button>
+                                </p>
+                            );
+                        }
+
+                    })}
+                    <h3>Marshell</h3>
+                    {marshell.map((data, index) => {
+                        if (data.checked) {
+                            return (
+                                <p>
+                                    <label>{data.name}</label>
+                                    <button type="button" onClick={() => this.ChangeValue(index, 'marshell')}>Clear</button>
+                                </p>
+                            );
+                        }
+                    })}
                 </div>
             </div>
         );
